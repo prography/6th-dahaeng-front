6df51dd71d299bf2103f3a80a569e8d76a950809
joohee <vkdlfl0224@naver.com>
2020-05-30 12:22:29 +0900

    Update sign css

--- src/components/AuthForm/index.js
+++ src/components/AuthForm/index.js
@@ -113,7 +113,7 @@ const AuthForm = ({ type, form, onChange, onSubmit, status }) => {
             type="password"
             onChange={onChange}
             value={form.password}
-          ></Input>}
+          ></Input>
 
           {type === 'sign' && (
             <>

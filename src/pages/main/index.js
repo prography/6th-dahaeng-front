a49c4323fad5ddf5ddc5560c302b0eabc405620a
강주히 <vkdlfl0224@naver.com>
2020-06-03 19:48:00 +0900

    Resolve merge conflict

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -9,6 +9,37 @@ const Main = () => {
       메인 페이지
       <Room></Room>
     </div>
+
+    //충돌난 부분
+    // <>
+    //   <Header></Header>
+    //   <Wrapper>
+    //     <QuestionBox>
+    //       <Date>{question.date}</Date>
+    //       <Question onClick={setModal}>{question.text}</Question>
+    //     </QuestionBox>
+    //     <Modal
+    //       openModal={openModal}
+    //       setModal={setModal}
+    //       title={
+    //         <ModalTitle>
+    //           <Date>{question.date}</Date>
+    //           <ModalQuestion>{question.text}</ModalQuestion>
+    //         </ModalTitle>
+    //       }
+    //       content={
+    //         <ModalContent>
+    //           <ModalCharacter></ModalCharacter>
+    //           <ModalInput value={inputText} onChange={onChange}></ModalInput>
+    //         </ModalContent>
+    //       }
+    //       button={
+    //         <ModalButton onClick={completeRecord}>행복 기록 완료</ModalButton>
+    //       }
+    //     ></Modal>
+    //   </Wrapper>
+    //   <Room></Room>
+    // </>
   );
 };
 

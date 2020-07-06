e306ae490c59f5cbd09189a4d1c5ab208babbf70
minjyo <minjyo8823@gmail.com>
2020-07-06 16:21:02 +0900

    feat: Add item store & saga

--- src/saga/user.js
+++ src/saga/user.js
@@ -1,48 +1,7 @@
 import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
 import { REMINDER, REMINDER_SUCCESS, REMINDER_FAIL } from 'store/user';
-import * as authApi from 'api/auth';
-import createRequestSaga from './createSaga';
+import { GETITEMS, GETITEMS_SUCCESS, GETITEMS_FAIL } from 'store/user';
 import axios from 'axios';
-
-// function* getUserSaga(action) {
-//   try {
-//     //call: Promise를 반환하는 함수 호출하고 기다림 (함수, 해당 함수에 넣을 인수)
-
-//     // const res = yield call(authApi.create, action.payload); //api.login(action.payload)와 같다
-
-//     console.log('rminder');
-//     // const headers = {
-//     //   Authorization: `jwt ${localStorage.getItem('accessToken')}`,
-//     // };
-//     const profile_id = getSelector(profile_id);
-
-//     const res = yield call(
-//       [axios, 'get'],
-//       `http://ec2-15-164-55-163.ap-northeast-2.compute.amazonaws.com:7878/profile/${profile_id}`,
-//       // { headers: headers },
-//     );
-
-//     console.log('response: ', res);
-//     if (res.response === 'success') {
-//       yield put({
-//         type: REMINDER_SUCCESS,
-//         payload: res.data.message,
-//       });
-//     } else {
-//       yield put({
-//         type: REMINDER_FAIL,
-//         payload: res.data.message,
-//         error: true,
-//       });
-//     }
-//   } catch (e) {
-//     yield put({
-//       type: REMINDER_FAIL,
-//       payload: e,
-//       error: true,
-//     });
-//   }
-// }
 
 function* reminderSaga(action) {
   try {
@@ -82,8 +41,46 @@ function* reminderSaga(action) {
   }
 }
 
+function* getItemSaga(action) {
+  try {
+    //call: Promise를 반환하는 함수 호출하고 기다림 (함수, 해당 함수에 넣을 인수)
+
+    // const res = yield call(authApi.create, action.payload); //api.login(action.payload)와 같다
+
+    console.log('getItmes');
+    // const headers = {
+    //   Authorization: `jwt ${localStorage.getItem('accessToken')}`,
+    // };
+    const res = yield call(
+      [axios, 'get'],
+      // 'http://ec2-15-164-55-163.ap-northeast-2.compute.amazonaws.com:7878/items',
+      // { headers: headers },
+    );
+
+    console.log('response: ', res);
+    if (res.response === 'success') {
+      yield put({
+        type: GETITEMS_SUCCESS,
+        payload: res.data.message,
+      });
+    } else {
+      yield put({
+        type: GETITEMS_FAIL,
+        payload: res.data.message,
+        error: true,
+      });
+    }
+  } catch (e) {
+    yield put({
+      type: GETITEMS_FAIL,
+      payload: e,
+      error: true,
+    });
+  }
+}
+
 export function* userSaga() {
   //yield takeLatest(GETUSER, getUserSaga);
   yield takeLatest(REMINDER, reminderSaga);
-  // yield takeLatest(GETUSER, getUserSaga);
+  yield takeLatest(GETITEMS, getItemSaga);
 }

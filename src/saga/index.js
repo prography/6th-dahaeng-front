ea6b96ca3ec13fa99428f61294fd9aba0a38b06c
minjyo <minjyo8823@gmail.com>
2020-07-08 17:20:05 +0900

    fix: Add serverURL var

--- src/saga/index.js
+++ src/saga/index.js
@@ -3,6 +3,9 @@ import { authSaga } from './auth';
 import { userSaga } from './user';
 import { boxSaga } from './box';
 
+export const serverURL =
+  'http://dahaengbackend-dev.ap-northeast-2.elasticbeanstalk.com/';
+
 export default function* rootSaga() {
   //all: 여러 사가를 합쳐줌
   yield all([fork(authSaga), fork(userSaga), fork(boxSaga)]);

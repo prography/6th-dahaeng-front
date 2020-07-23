b87752ae4e77775bc378a9d8df89053bd80b504e
minjyo <minjyo8823@gmail.com>
2020-07-23 10:39:41 +0900

    Update serverURL

--- src/saga/index.js
+++ src/saga/index.js
@@ -4,7 +4,8 @@ import { userSaga } from './user';
 import { boxSaga } from './box';
 
 export const serverURL =
-  'http://ec2-15-165-219-116.ap-northeast-2.compute.amazonaws.com/';
+  //'http://ec2-15-165-219-116.ap-northeast-2.compute.amazonaws.com';
+  'https://www.dahaengback.shop';
 
 export default function* rootSaga() {
   //all: 여러 사가를 합쳐줌

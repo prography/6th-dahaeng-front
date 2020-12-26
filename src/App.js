26929d3202dd507febdc29325c9e998ea6438cab
minjyo <minjyo8823@gmail.com>
2020-12-26 14:52:07 +0900

    feat: Get firebase token from app to webview

--- src/App.js
+++ src/App.js
@@ -24,30 +24,29 @@ ReactGA.pageview(window.location.pathname + window.location.search);
 
 const RNListener = () => {
   /** react native 환경에서만 가능 */
-  const listener = (event) => {
-    const { data, type } = JSON.parse(event.data);
-    if (type === 'TOKEN') {
-      // type이 TOKEN이기 때문에 이곳에 콘솔이 찍히게 됩니다.
-      console.log(data); // xxxxx
-    } else if (type === 'NOTIFICATION') {
-    }
-  };
-
-  if (window.ReactNativeWebView) {
-    /** android */
-    document.addEventListener('message', listener);
-    /** ios */
-    window.addEventListener('message', listener);
-  } else {
-    // 모바일이 아니라면 모바일 아님을 alert로 띄웁니다.
-    alert({ message: 'not mobile' });
-  }
+  // const listener = (event) => {
+  //   const { data, type } = JSON.parse(event.data);
+  //   if (type === 'TOKEN') {
+  //     // type이 TOKEN이기 때문에 이곳에 콘솔이 찍히게 됩니다.
+  //     console.log(data); // xxxxx
+  //   } else if (type === 'NOTIFICATION') {
+  //   }
+  // };
+  // if (window.ReactNativeWebView) {
+  //   /** android */
+  //   document.addEventListener('message', listener);
+  //   /** ios */
+  //   window.addEventListener('message', listener);
+  // } else {
+  //   // 모바일이 아니라면 모바일 아님을 alert로 띄웁니다.
+  //   alert({ message: 'not mobile' });
+  // }
 };
 
 function App() {
   return (
     <BrowserRouter>
-      <RNListener></RNListener>
+      {/* <RNListener></RNListener> */}
       {/* <Header></Header> */}
       <Suspense fallback={<Loading />}>
         <Switch>
